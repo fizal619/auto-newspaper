01240f76b6c3be8538d83502daf9bb87e2f7800c
Fizal Sarif <fizal.sarif@gmail.com>
2016-07-30 17:25:19 -0400

    set up rails and made scraper logic

---
+++ auto_newspaper_app/app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+
+  def index
+    #check the current time's hour value
+    d = DateTime.parse(Time.now.to_s)
+    articles = []
+      if d.hour < 6
+
+      # nuke the table
+      Article.destroy_all
+
+      # grab the tech page
+      response = HTTParty.get('http://www.huffingtonpost.com/news/tech/')
+      page = Nokogiri::HTML(response.body)
+
+      # loop through the links and do something.
+      links =  page.css('a.read_post_link')
+      for link in links do
+        # articles.push(link.attributes['href'].value)
+        response = HTTParty.get(links[0].attributes['href'].value)
+        page = Nokogiri::HTML(response.body)
+
+        # where we'll store the paragraphs
+        parsedBody = ''
+
+        #ignore brs and unneded linebreaks?
+        page.css('#\31 1256816 > div.entry__container > div > div.entry__body.js-entry-body > div:nth-child(1) > p').children.each {
+          |item|
+          if item.text != '' && item.text != '\n '
+            parsedBody += item.text
+          end
+        }
+
+        # build and object
+        article = {
+          title: page.css('h1.headline__title').children[0].text,
+          content: parsedBody ,
+          link: link
+        }
+        # pp article[:body]
+        articles.push(article)
+
+        #save them to the database
+        Article.create(articles)
+
+      end #end for
+    end #end if
+
+    render json: Article.all
+  end
+
+  def destroy
+
+  end
+
+end
---
+++ auto_newspaper_app/app/controllers/topics_controller.rb
@@ -0,0 +1,24 @@
+class TopicsController < ApplicationController
+
+  def index
+
+  end
+
+
+  def create
+
+  end
+
+
+  def update
+
+  end
+
+
+  def destroy
+
+  end
+
+
+
+end
