3045425ba6c73d87aed396919ddfca97d7cb1a44
Fizal Sarif <fizal.sarif@gmail.com>
2016-08-01 14:14:02 -0400

    cors fixed?

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,11 @@ module AutoNewspaperApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-config.middleware.insert_before 0, "Rack::Cors" do
-  allow do
-    origins '*'
-    resource '*', :headers => :any, :methods => [:get, :post, :options]
-  end
-end
-
+    config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => 'GET, PATCH, PUT, POST, OPTIONS, DELETE'
+    'Access-Control-Allow-Headers:' => 'Origin, X-Requested-With, Content-Type, Accept'
+    }
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  match '(:anything)' => 'application#nothing', via: [:options]
+
+
   get           '/news',        to: 'articles#index'
   delete        '/news/:id',    to: 'articles#destroy'
 
